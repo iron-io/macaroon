de2ea9ae7d4a01f4c3fdbb1f23c87200e4f833ba
thousandsofthem <thousandsofthem@gmail.com>
2015-02-04 21:04:55 +0300

    first version

--- marshal.go
+++ marshal.go
@@ -1,10 +1,13 @@
 package macaroon
 
 import (
-	"encoding/base64"
+	"bytes"
+	"encoding/binary"
 	"encoding/hex"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"reflect"
 )
 
 type field byte
@@ -15,9 +18,8 @@ const (
 	fieldLocation
 	fieldIdentifier
 	fieldSignature
-	fieldCaveatId
+	fieldCaveat
 	fieldVerificationId
-	fieldCaveatLocation
 )
 
 var fieldStrings = [...]string{
@@ -25,9 +27,8 @@ var fieldStrings = [...]string{
 	fieldLocation:       "location",
 	fieldIdentifier:     "identifier",
 	fieldSignature:      "signature",
-	fieldCaveatId:       "cid",
+	fieldCaveat:         "cav",
 	fieldVerificationId: "vid",
-	fieldCaveatLocation: "cl",
 }
 
 func (f field) String() string {
@@ -39,10 +40,10 @@ func (f field) String() string {
 
 // macaroonJSON defines the JSON format for macaroons.
 type macaroonJSON struct {
-	Caveats    []caveatJSON `json:"caveats"`
-	Location   string       `json:"location"`
-	Identifier string       `json:"identifier"`
-	Signature  string       `json:"signature"` // hex-encoded
+	Caveats    string `json:"caveats"`
+	Location   string `json:"location"`
+	Identifier string `json:"identifier"`
+	Signature  string `json:"signature"` // hex-encoded
 }
 
 // caveatJSON defines the JSON format for caveats within a macaroon.
@@ -58,14 +59,7 @@ func (m *Macaroon) MarshalJSON() ([]byte, error) {
 		Location:   m.Location(),
 		Identifier: m.dataStr(m.id),
 		Signature:  hex.EncodeToString(m.sig),
-		Caveats:    make([]caveatJSON, len(m.caveats)),
-	}
-	for i, cav := range m.caveats {
-		mjson.Caveats[i] = caveatJSON{
-			Location: m.dataStr(cav.location),
-			CID:      m.dataStr(cav.caveatId),
-			VID:      base64.StdEncoding.EncodeToString(m.dataBytes(cav.verificationId)),
-		}
+		Caveats:    hex.EncodeToString(m.caveatsRaw()),
 	}
 	data, err := json.Marshal(mjson)
 	if err != nil {
@@ -77,6 +71,7 @@ func (m *Macaroon) MarshalJSON() ([]byte, error) {
 // UnmarshalJSON implements json.Unmarshaler.
 func (m *Macaroon) UnmarshalJSON(jsonData []byte) error {
 	var mjson macaroonJSON
+	var ok bool
 	err := json.Unmarshal(jsonData, &mjson)
 	if err != nil {
 		return fmt.Errorf("cannot unmarshal json data: %v", err)
@@ -88,16 +83,15 @@ func (m *Macaroon) UnmarshalJSON(jsonData []byte) error {
 	if err != nil {
 		return fmt.Errorf("cannot decode macaroon signature %q: %v", m.sig, err)
 	}
-	m.caveats = m.caveats[:0]
-	for _, cav := range mjson.Caveats {
-		vid, err := base64.StdEncoding.DecodeString(cav.VID)
-		if err != nil {
-			return fmt.Errorf("cannot decode verification id %q: %v", cav.VID, err)
-		}
-		if _, err := m.appendCaveat(cav.CID, vid, cav.Location); err != nil {
-			return err
-		}
+	cavData, err := hex.DecodeString(mjson.Caveats)
+	if err != nil {
+		return fmt.Errorf("cannot decode macaroon caveats %q: %v", m.caveats, err)
 	}
+	m.caveats, ok = m.appendPacket(fieldCaveat, cavData)
+	if !ok {
+		return fmt.Errorf("cannot decode macaroon caveats")
+	}
+
 	return nil
 }
 
@@ -135,42 +129,12 @@ func (m *Macaroon) unmarshalBinaryNoCopy(data []byte) error {
 	if err != nil {
 		return err
 	}
-	var cav caveat
-	for {
-		p, err := m.parsePacket(start)
-		if err != nil {
-			return err
-		}
-		start += p.len()
-		switch f := m.fieldNum(p); f {
-		case fieldSignature:
-			// At the end of the caveats we find the signature.
-			if cav.caveatId.len() != 0 {
-				m.caveats = append(m.caveats, cav)
-			}
-			// Remove the signature from data.
-			m.data = m.data[0:p.start]
-			m.sig = append([]byte(nil), m.dataBytes(p)...)
-			return nil
-		case fieldCaveatId:
-			if cav.caveatId.len() != 0 {
-				m.caveats = append(m.caveats, cav)
-			}
-			cav.caveatId = p
-		case fieldVerificationId:
-			if cav.verificationId.len() != 0 {
-				return fmt.Errorf("repeated field %v in caveat", fieldVerificationId)
-			}
-			cav.verificationId = p
-		case fieldCaveatLocation:
-			if cav.location.len() != 0 {
-				return fmt.Errorf("repeated field %v in caveat", fieldLocation)
-			}
-			cav.location = p
-		default:
-			return fmt.Errorf("unexpected field %x", f)
-		}
+	start, m.caveats, err = m.expectPacket(start, fieldCaveat)
+	if err != nil {
+		return err
 	}
+
+	return nil
 }
 
 // UnmarshalBinary implements encoding.BinaryUnmarshaler.
@@ -242,4 +206,191 @@ func (s *Slice) UnmarshalBinary(data []byte) error {
 		data = data[m.marshalBinaryLen():]
 	}
 	return nil
+}
+
+// convert data struct into []byte
+func MarshalCaveats(v interface{}) ([]byte, error) {
+	obj := reflect.ValueOf(v)
+
+	var out bytes.Buffer
+
+	for i := 0; i < obj.NumField(); i += 1 {
+		val := obj.Field(i)
+
+		switch val.Kind() {
+		case reflect.Ptr:
+			if val.IsNil() {
+				// ignore
+			} else {
+				data, err := valToBytes(val.Elem())
+				if err != nil {
+					return []byte{}, err
+				}
+
+				out.Write([]byte{byte(uint8(i))})  // Field Num
+				out.Write([]byte{byte(len(data))}) // Field Size
+				out.Write(data)                    // Field data itself
+			}
+		default:
+			panic("non-pointer type, please check structure field types")
+		}
+
+	}
+
+	return out.Bytes(), nil
+}
+
+func UnMarshalCaveats(v interface{}, data []byte) error {
+	dataLen := len(data)
+
+	obj := reflect.ValueOf(v).Elem()
+	typeOfStruct := obj.Type()
+
+	cursor := 0
+	for cursor < dataLen {
+		fieldNum := uint8(data[cursor])
+		dataSize := uint8(data[cursor+1])
+
+		fieldData := data[cursor+2 : cursor+2+int(dataSize)]
+
+		t := typeOfStruct.Field(int(fieldNum)).Type
+
+		val := obj.Field(int(fieldNum))
+
+		value, err := bytesToVal(t, fieldData)
+
+		if err != nil {
+			return err
+		}
+
+		val.Set(value)
+
+		cursor += 2 + int(dataSize)
+	}
+	return nil
+}
+
+func valToBytes(val reflect.Value) ([]byte, error) {
+	switch val.Kind() {
+	case reflect.Int, reflect.Uint:
+		panic("please always specify exact size, like int8 or uint32")
+
+	case reflect.Slice:
+		return val.Bytes(), nil
+
+	case reflect.String:
+		return []byte(val.String()), nil
+
+	case reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		buf := new(bytes.Buffer)
+		var err error
+
+		// TODO: refactor somehow
+		switch val.Kind() {
+		case reflect.Int8:
+			err = binary.Write(buf, binary.LittleEndian, int8(val.Int()))
+		case reflect.Int16:
+			err = binary.Write(buf, binary.LittleEndian, int16(val.Int()))
+		case reflect.Int32:
+			err = binary.Write(buf, binary.LittleEndian, int32(val.Int()))
+		case reflect.Int64:
+			err = binary.Write(buf, binary.LittleEndian, val.Int())
+		case reflect.Uint8:
+			err = binary.Write(buf, binary.LittleEndian, uint8(val.Uint()))
+		case reflect.Uint16:
+			err = binary.Write(buf, binary.LittleEndian, uint16(val.Uint()))
+		case reflect.Uint32:
+			err = binary.Write(buf, binary.LittleEndian, uint32(val.Uint()))
+		case reflect.Uint64:
+			err = binary.Write(buf, binary.LittleEndian, val.Uint())
+		default:
+			panic("bug in the code")
+		}
+		return buf.Bytes(), err
+
+	case reflect.Bool:
+		value := val.Bool()
+		if value {
+			return []byte{1}, nil
+		} else {
+			return []byte{0}, nil
+		}
+	default:
+		panic("Unsupported type, please check structure field types")
+	}
+
+	var data []byte
+	data = make([]byte, 5, 5)
+
+	return data, nil
+}
+
+func bytesToVal(val reflect.Type, data []byte) (reflect.Value, error) {
+	var err error
+
+	switch val.Elem().Kind() {
+	case reflect.Int, reflect.Uint:
+		panic("please always specify exact size, like int8 or uint32")
+
+	case reflect.Slice:
+		return reflect.ValueOf(&data), nil
+
+	case reflect.String:
+		str := string(data)
+		return reflect.ValueOf(&str), nil
+
+	case reflect.Bool:
+		var value bool
+		if data[0] == 1 {
+			value = true
+		} else if data[0] == 0 {
+			value = false
+		} else {
+			err = errors.New("can not decode input data")
+		}
+		return reflect.ValueOf(&value), err
+
+	// TODO: refactor somehow
+	case reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		buf := bytes.NewReader(data)
+		switch val.Elem().Kind() {
+		case reflect.Int8:
+			var n int8
+			err = binary.Read(buf, binary.LittleEndian, &n)
+			return reflect.ValueOf(&n), err
+		case reflect.Int16:
+			var n int16
+			err = binary.Read(buf, binary.LittleEndian, &n)
+			return reflect.ValueOf(&n), err
+		case reflect.Int32:
+			var n int32
+			err = binary.Read(buf, binary.LittleEndian, &n)
+			return reflect.ValueOf(&n), err
+		case reflect.Int64:
+			var n int64
+			err = binary.Read(buf, binary.LittleEndian, &n)
+			return reflect.ValueOf(&n), err
+		case reflect.Uint8:
+			var n uint8
+			err = binary.Read(buf, binary.LittleEndian, &n)
+			return reflect.ValueOf(&n), err
+		case reflect.Uint16:
+			var n uint16
+			err = binary.Read(buf, binary.LittleEndian, &n)
+			return reflect.ValueOf(&n), err
+		case reflect.Uint32:
+			var n uint32
+			err = binary.Read(buf, binary.LittleEndian, &n)
+			return reflect.ValueOf(&n), err
+		case reflect.Uint64:
+			var n uint64
+			err = binary.Read(buf, binary.LittleEndian, &n)
+			return reflect.ValueOf(&n), err
+		default:
+			panic("bug in the code")
+		}
+
+	default:
+		return reflect.ValueOf(nil), errors.New("unknown field type")
+	}
 }
